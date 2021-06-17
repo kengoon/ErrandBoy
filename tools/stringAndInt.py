6215aa28045fefcf45eb2b3ce60c4a1780f570cd
sirkelly-B <okaforpatrick2021@gmail.com>
2021-06-17 13:05:57 -0700

    program converting string or integer to list continuously stringAndInt.py

--- tools/stringAndInt.py
+++ tools/stringAndInt.py
@@ -1,8 +1,6 @@
 # program converting string or integer to list continuously
 def string_int(string):
-    string = str(string)
-    return list(string)
-
+    return list(str(string))
 
 if __name__ == '__main__':
     print(string_int(1237893444787600.00))
