f8c08dca3d8f4e4c5f1cb6f3b8d8d41976dc9120
sirkelly-B <okaforpatrick2021@gmail.com>
2021-07-02 12:03:50 -0700

    timer.py - a program for timing a thread(s)

--- tools/timer.py
+++ tools/timer.py
@@ -1,17 +1,27 @@
 import time
 
 
-def hello():
+def call_me_back(*args):
+    """accepts and prints as when argument as possible"""
     print('Hello and welcome to Python\'s world oof programming')
     print('Welcome to the programming world, where we eat python (not the snake!!!) as food')
+    print(args[1], args)
 
 
-def timer():
+def the_timer(timer, callback=None):
+    """the_timer(timer, callback) - argument timer contains the seconds you want the program to run and callback is the
+    function you wish to callback."""
+    if not callable(callback):
+        raise NotImplementedError("'callback' is not a function")
+    start_time = 0
     while True:
-        hello()
-        time.sleep(2)  # argument taken is equal to the number of seconds the execution can be delayed
+        callback('hi', 'hello')
+        if start_time == timer:
+            break
+        time.sleep(1)  # argument taken is equal to the number of seconds the execution can be delayed
+        start_time += 1
 
 
 if __name__ == '__main__':
-    timer()
+    the_timer(10, call_me_back)
 
